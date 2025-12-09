f8c71263716fcdeb73ec83efe5efba8b015b6be0
gkibria121 <gkibria121@gmail.com>
2025-12-09 17:36:08 +0600

    refactor: update dataset download process and create symlink

--- download_dataset.py
+++ download_dataset.py
@@ -1,13 +1,22 @@
-"""
-AASIST
-Copyright (c) 2021-present NAVER Corp.
-MIT license
-"""
+# IMPORTANT: RUN THIS CELL IN ORDER TO IMPORT YOUR KAGGLE DATA SOURCES,
+# THEN FEEL FREE TO DELETE THIS CELL.
+# NOTE: THIS NOTEBOOK ENVIRONMENT DIFFERS FROM KAGGLE'S PYTHON
+# ENVIRONMENT SO THERE MAY BE MISSING LIBRARIES USED BY YOUR
+# NOTEBOOK.
+import kagglehub
+awsaf49_asvpoof_2019_dataset_path = kagglehub.dataset_download('awsaf49/asvpoof-2019-dataset')
 
+print('Data source import complete.')
 import os
 
-if __name__ == "__main__":
-    cmd = "curl -o ./LA.zip -# https://datashare.ed.ac.uk/bitstream/handle/10283/3336/LA.zip\?sequence\=3\&isAllowed\=y"
-    os.system(cmd)
-    cmd = "unzip LA.zip"
-    os.system(cmd)
+src = os.path.join(awsaf49_asvpoof_2019_dataset_path, "LA", "LA")  # real folder
+dst = "./LA"  # symlink to create in current directory
+
+# Remove old symlink or folder if exists
+if os.path.islink(dst) or os.path.exists(dst):
+    os.remove(dst)
+
+# Create symlink
+os.symlink(src, dst, target_is_directory=True)
+
+print("Symlink created:", dst, "→", src)
